35dd5aafe08ca6244ce0ab54a74dd15bb530fc33
anikamaji <majia@purdue.edu>
2025-01-26 09:26:28 -0500

    added dictionary entry finder

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 # next - match number to corresponding dictionary values; display (product_database.ipynb)
 # next - use values to get info about product sustainability and potential allergens (product_info.py, allergen_info.py)
 # next - use values to get info about brand sustainability (brand_info.py)
+import product_database
+import allergen_info
+import brand_info
+import product_info
+
+def main():
+   product_entry = product_database.get_database_entry(577606577902)
+   product_brand = product_entry[0]
+   product_type = product_entry[1]
+   product_materials = product_entry[2]
---
+++ product_database.py
@@ -0,0 +1,55 @@
+products = {
+    374440989658: {
+        "Brand": "Prada",
+        "Product type": "Sunglasses",
+        "Materials": {
+            "Acetate": 100
+        }
+    },
+    240335076189: {
+        "Brand": "Zara",
+        "Product type": "Sweatshirt",
+        "Materials": {
+            "Cotton": 85,
+            "Web cotton": 15
+        }
+    },
+    588879154298: {
+        "Brand": "Levi's",
+        "Product type": "Jeans",
+        "Materials": {
+            "Cotton": 85,
+            "Lyocell": 7,
+            "Elastomultiester": 6,
+            "Elastane": 2
+        }
+    },
+    111120732189: {
+        "Brand": "Forever 21",
+        "Product type": "Dress",
+        "Materials": {
+            "Polyester": 73,
+            "Rayon": 26,
+            "Nylon": 1
+        }
+    },
+    577606577902: {
+        "Brand": "H&M",
+        "Product type": "Scarf",
+        "Materials": {
+            "Recycled polyester": 100
+        }
+    }
+}
+
+def get_database_entry(barcode_num):
+  
+  product_info = products[barcode_num]
+  brand = product_info["Brand"]
+  type = product_info["Product type"]
+  materials = product_info["Materials"]
+
+  return brand, type, materials
+
+print(get_database_entry(577606577902))
+
