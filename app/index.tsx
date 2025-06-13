1a9f3575e23ccf67e5dcf8f31ef13fc8f0127d75
balan2 <balan2@purdue.edu>
2025-06-13 19:10:16 -0400

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import vestiLogo from '../assets/images/vesti-logo.png';
+
+const VestiCircleLogo = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.circleWrapper}>
+        <View style={styles.circle} />
+        <View style={[styles.circle, styles.overlappingCircle]} />
+        <Image source={vestiLogo} style={styles.image} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingBottom: 100,
+  },
+  circleWrapper: {
+    width: 200,
+    height: 200,
+    position: 'relative',
+    marginLeft: 22, // Shift right to visually center with overlap
+  },
+  circle: {
+    width: 180,
+    height: 180,
+    borderRadius: 90,
+    backgroundColor: '#F4BADC',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  overlappingCircle: {
+    top: -6,
+    left: -6,
+    backgroundColor: '#F7D2E8',
+  },
+    image: {
+    position: 'absolute',
+    top: 21,
+    left: 20,
+    width: 132,
+    height: 132,
+    resizeMode: 'contain', // or 'cover' if you want it to fill more
+  },
+});
+
+export default VestiCircleLogo;
+
